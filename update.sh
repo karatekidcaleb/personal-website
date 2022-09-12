1342dec0cb6b32a143041d92d1ed4f45a7ee3b7c
Caleb Leeb <78173621+karatekidcaleb@users.noreply.github.com>
2022-09-12 14:18:49 -0400

    ...
    ...

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 shopt -s extglob
 cd ..
-rm -fr !("composer.json"|"s3test.php"|"composer.lock"|"vendor"|"update.sh")
+rm -fr !("composer.json"|"s3test.php"|"composer.lock"|"vendor"|"update.sh"|"personal-website")
 cd personal-website
 cp -r !(*.sh) ..
 cd ..
