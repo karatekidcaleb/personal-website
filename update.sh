7b89db44334fc2db7d8aa5af3c4564768948c13a
Caleb Leeb <78173621+karatekidcaleb@users.noreply.github.com>
2022-09-12 14:12:24 -0400

    Final directory fix for Updater
    Scopes properly now and includes all subdirectories. Self-deleting version

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 shopt -s extglob
+cd ..
 rm -fr !("composer.json"|"s3test.php"|"composer.lock"|"vendor"|"update.sh")
 git clone https://github.com/karatekidcaleb/personal-website.git
 cd personal-website
-cp -r * ..
+cp -r !(*.sh) ..
 cd ..
 rm -fr personal-website
 echo "Webserver updated, exit status:" $?
